795952d906b23e236e22997b87b64643296063d4
Julio Bermudez <jbermudezfc90@gmail.com>
2020-05-06 12:09:52 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+
+http.createServer((req, res) => {
+    const q = url.parse(req.url, true);
+    const file = q.pathname !== '/' ? `.${q.pathname}.html` : 'index.html';
+    console.log(q.pathname);
+    fs.readFile(file, (err, data) => {
+        if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            return res.end(`<h1>404 not found</h1> <a href='/'> back to home</a>`);
+        }
+        res.writeHead(200, { 'Content-type': 'text/html' });
+        res.write(data);
+        return res.end();
+    });
+}).listen(8000);
